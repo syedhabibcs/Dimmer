72c0bd535ba29c0b039a3d342478a99128df5944
ohin94 <nibras.ohin@invenia.ca>
2018-04-17 21:29:33 -0500

    Added chart api for lux

--- server.py
+++ server.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template, request
 import json
 import requests
-
+from datetime import datetime
+import threading
+import time
 
 class Server:
 
     # This will be set from the user input from the web client
     lux_svalue = 0
     led_brightness = 50
+
+    chart_seconds = []
 
     def run(self):
         app = Flask(__name__)
@@ -39,6 +43,25 @@ class Server:
         def getFluxValue():
             return json.dumps({'lux_value': str(Server.lux_svalue)})
 
+        @app.route("/chart/",methods = ["GET","POST"])
+        def getChartValue():
+            # Server.chart_seconds.append((1,2))
+            # Server.chart_seconds.append((3,4))
+
+            chart_dic = {'seconds': Server.chart_seconds}
+            return json.dumps(chart_dic)
+
+    def createChartValueSeconds(self):
+        for i in range(0,12):
+            # Server.chart_seconds.append((datetime.now().strftime('%H:%M:%S'), Server.lux_svalue))
+            Server.chart_seconds.append((int(time.time()), Server.lux_svalue))
+            time.sleep(5)
+        while True:
+            Server.chart_seconds.pop(0)
+            Server.chart_seconds.append((int(time.time()), Server.lux_svalue))
+            time.sleep(5)
+
+
     def setLedBrightness(self, led_brightness):
             Server.led_brightness = led_brightness
 
@@ -46,6 +69,9 @@ if __name__ == '__main__':
     server = Server()
     app =  server.run()
     server.routes()
+    thread = threading.Thread(target=server.createChartValueSeconds, args=())
+    thread.daemon = True                            # Daemonize thread
+    thread.start()
     app.run(debug=True, host='0.0.0.0')
 
 
